1c2d533d53c878837bf8be71b5db1bf7cb057d50
Jeremy <adgrygry12348@gmail.com>
2020-09-11 09:55:48 +0700

    Add

--- app/src/main/java/com/example/aiproject/MainActivity.java
+++ app/src/main/java/com/example/aiproject/MainActivity.java
@@ -6,8 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity {
-    Button b0,b1,b2,b3,b4,b5,b6,b7,b8,bSolve,bReset;
+    /*
+    * Initialize Variabels to be used
+    * */
+    protected Button b0,b1,b2,b3,b4,b5,b6,b7,b8,bSolve,bReset;
+    protected Integer[] numbers = {0,1,2,3,4,5,6,7,8};
+    protected Integer[] boards = {-1,-1,-1,-1,-1,-1,-1,-1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +34,78 @@ public class MainActivity extends AppCompatActivity {
         b8 = findViewById(R.id.button8);
         bSolve = findViewById(R.id.buttonSolve);
         bReset = findViewById(R.id.buttonReset);
+
+        Reset();
     }
 
     public void butClick(View v)
     {
+        /*
+        * Get every button pressed id and filter the button
+        * */
         int id = v.getId();
-        if (id == R.id.button0)
+        if(id == R.id.buttonSolve)
         {
-            b0.setText("Anu");
+            Solve();
+        }
+        else if (id == R.id.buttonReset)
+        {
+            Reset();
+        }
+        else
+        {
+            /*
+            * Temporary var to get the button text
+            * */
+            Button b = (Button) v;
+            String num = b.getText().toString();
+
+            /*
+            * if its not zero check up down left right
+            * */
+            if (!num.equalsIgnoreCase("0"))
+            {
+
+            }
         }
     }
 
+    public void Solve()
+    {
+        /*
+        * To Be Added Solver function
+        * BFS
+        * DFS
+        * (+)
+        * */
+    }
+
+    public void Reset()
+    {
+        /*
+        * Reset State of button and assign new number for each Button
+        * */
+        // Random Every Number in a List
+        Collections.shuffle(Arrays.asList(numbers));
+
+        boards[0] = numbers[0];
+        boards[1] = numbers[1];
+        boards[2] = numbers[2];
+        boards[3] = numbers[3];
+        boards[4] = numbers[4];
+        boards[5] = numbers[5];
+        boards[6] = numbers[6];
+        boards[7] = numbers[7];
+        boards[8] = numbers[8];
+
+        b0.setText(boards[0]);
+        b1.setText(boards[1]);
+        b2.setText(boards[2]);
+        b3.setText(boards[3]);
+        b4.setText(boards[4]);
+        b5.setText(boards[5]);
+        b6.setText(boards[6]);
+        b7.setText(boards[7]);
+        b8.setText(boards[8]);
+    }
 }
